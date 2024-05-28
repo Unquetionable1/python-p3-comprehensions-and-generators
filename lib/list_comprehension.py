351f13d4f125215699a29440b37a18f8bea8f7ea
Edmound Njoori <eddnjoori@gmail.com>
2024-05-28 15:28:05 +0300

    Uses comprehention

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-
+num_list=[1,2]
 def return_evens(num_list):
-    pass
+    # Correct list comprehension to filter out even numbers
+    evens = [num for num in num_list if num % 2 == 0]
+    return evens
+
+
+print(return_evens(num_list))
+
 
 def make_exclamation(sentence_list):
-    pass
+    
+    return [sent +'!' for sent in sentence_list  if len(sentence_list) > 0 ]
+print(make_exclamation(sentence_list=['s','m','n']))
